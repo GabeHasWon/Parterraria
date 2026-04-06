daf159e76532703af15dd7ffee1e506b12700858
GabeHasWon <gabehw.contact@gmail.com>
2026-04-06 00:45:52 -0500

    start world presets UI

---
+++ Core/WorldPresets/WorldPresetHooking.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Terraria.Audio;
+using Terraria.GameContent.UI.Elements;
+using Terraria.GameContent.UI.States;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.UI;
+
+namespace Parterraria.Core.WorldPresets;
+
+internal class WorldPresetHooking : ILoadable
+{
+    public void Load(Mod mod)
+    {
+        On_UIWorldCreation.MakeBackAndCreatebuttons += MakePresetButton;
+    }
+
+    private void MakePresetButton(On_UIWorldCreation.orig_MakeBackAndCreatebuttons orig, UIWorldCreation self, UIElement outerContainer)
+    {
+        orig(self, outerContainer);
+
+        UITextPanel<LocalizedText> uITextPanel2 = new UITextPanel<LocalizedText>(Language.GetText("Mods.Parterraria.MiscUI.PresetWorlds.Title"), 0.7f, large: true)
+        {
+            Width = StyleDimension.FromPixelsAndPercent(-10, 0.5f),
+            Height = StyleDimension.FromPixels(50f),
+            VAlign = 1f,
+            HAlign = 0.5f,
+            Top = StyleDimension.FromPixels(10)
+        };
+
+        uITextPanel2.OnMouseOver += FadedMouseOver;
+        uITextPanel2.OnMouseOut += FadedMouseOut;
+        uITextPanel2.OnLeftMouseDown += (_, _) => SetToPresetUI(self);
+        uITextPanel2.SetSnapPoint("Presets", 0);
+        outerContainer.Append(uITextPanel2);
+    }
+
+    private void SetToPresetUI(UIWorldCreation self) => Main.MenuUI.SetState(new WorldPresetState(() => Main.MenuUI.SetState(self)));
+
+    private void FadedMouseOver(UIMouseEvent evt, UIElement listeningElement)
+    {
+        SoundEngine.PlaySound(SoundID.MenuTick);
+        ((UIPanel)evt.Target).BackgroundColor = new Color(73, 94, 171);
+        ((UIPanel)evt.Target).BorderColor = Colors.FancyUIFatButtonMouseOver;
+    }
+
+    private void FadedMouseOut(UIMouseEvent evt, UIElement listeningElement)
+    {
+        ((UIPanel)evt.Target).BackgroundColor = new Color(63, 82, 151) * 0.8f;
+        ((UIPanel)evt.Target).BorderColor = Color.Black;
+    }
+
+    public void Unload() { }
+}
---
+++ Core/WorldPresets/WorldPresetState.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.UI.Elements;
+using Terraria.Localization;
+using Terraria.ModLoader.UI;
+using Terraria.UI;
+
+namespace Parterraria.Core.WorldPresets;
+
+internal class WorldPresetState(Action returnAction) : UIState
+{
+    private readonly Action _returnAction = returnAction;
+
+    public override void OnInitialize()
+    {
+        const int Buffer = 18;
+
+        UIElement hiddenElement = new()
+        {
+            Width = StyleDimension.FromPixels(500f),
+            Height = StyleDimension.FromPixels(484f + Buffer),
+            Top = StyleDimension.FromPixels(170f - Buffer),
+            HAlign = 0.5f,
+            VAlign = 0f
+        };
+        hiddenElement.SetPadding(0f);
+        Append(hiddenElement);
+
+        UIPanel panel = new()
+        {
+            Width = StyleDimension.FromPercent(1f),
+            Height = StyleDimension.FromPixels(360 + Buffer),
+            Top = StyleDimension.FromPixels(50f),
+            BackgroundColor = new Color(33, 43, 79) * 0.8f
+        };
+        panel.SetPadding(12);
+        hiddenElement.Append(panel);
+
+        UIButton<string> backButton = new("x")
+        {
+            Width = StyleDimension.FromPixels(32),
+            Height = StyleDimension.FromPixels(32),
+            HAlign = 1,
+            VAlign = 0,
+        };
+
+        backButton.OnLeftClick += (_, _) => _returnAction();
+        panel.Append(backButton);
+
+        UIText title = new(Language.GetText("Mods.Parterraria.MiscUI.PresetWorlds.Title"), 0.8f, true);
+        panel.Append(title);
+
+        AddListUI(panel);
+    }
+
+    private void AddListUI(UIPanel panel)
+    {
+        UIPanel listPanel = new()
+        {
+            Width = StyleDimension.Fill,
+            Height = StyleDimension.FromPixelsAndPercent(-80, 1),
+            Top = StyleDimension.FromPixels(36),
+        };
+
+        panel.Append(listPanel);
+    }
+}
